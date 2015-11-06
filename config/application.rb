91d143657416edd0a890439542e613127ebea207
aarnwri <aarnwri@gmail.com>
2015-11-05 20:58:25 -0800

    fixed 'start-over-name'

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CivRemakeApi2
+module CivRemakeApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -31,7 +31,7 @@ module CivRemakeApi2
         :routing_specs => false,
         :controller_specs => true,
         :request_specs => true
-        
+
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
   end
