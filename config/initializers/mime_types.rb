30ae54aa4551e3d8ba636c895e49c1ba63287960
aarnwri <aarnwri@gmail.com>
2015-11-27 12:47:43 -0800

    added application/vdn.api+json mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,12 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+json_api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, json_api_mime_types
