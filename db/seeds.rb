63f9918dfde2ad8bc85ef598e0fb15e6626c1b3c
aarnwri <aarnwri@gmail.com>
2015-12-11 15:52:31 -0800

    added some users to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+("a".."e").each do |char|
+  User.create(email: "#{char}@#{char}.com", password: "password")
+end
